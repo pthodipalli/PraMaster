f4977368c5fd45a3b69000fd9bc3d3ebcd4e25ec
rajes <rajes@DESKTOP-G04DR6E.modem>
2023-08-01 14:18:01 +1000

    Created new project for team

---
+++ src/test/java/practise/TestDemo.java
@@ -0,0 +1,17 @@
+package practise;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+
+
+
+public class TestDemo 
+{
+ 	@Test 
+ 	private void simple() 
+ 	{
+		Reporter.log("Hi Prasanthi");
+
+	}
+	
+	
+}
